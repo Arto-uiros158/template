4c29e7366b673387e524082e8a97ed96d8549fd6
Arto-uiros158 <arthurshubin1994@yandex.ru>
2020-05-19 20:22:01 +0300

    Добавил шаблон для выпадающего меню

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($) {
 
 });
 
+
 //===========================================================
 // IMAGE-BACKGROUND (IBG)
 // function ibg(){
@@ -71,3 +72,35 @@ jQuery(document).ready(function($) {
 // 	}
 // 	$(this).toggleClass('active').next().slideToggle(300);
 // });
+
+//===========================================================
+// DROP-DOWN MENU
+// let isMobile = {
+//     Android: function() { return navigator.userAgent.match(/Android/i); },
+//     BlackBerry: function() { return navigator.userAgent.match(/BlackBerry/i); },
+//     iOS: function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+//     Opera: function() { return navigator.userAgent.match(/Opera Mini/i); },
+//     Windows: function() { return navigator.userAgent.match(/IEMobile/i); },
+//     any: function() { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+// }; /* определяет является ли устройство мобильным (ключевое: есть тачскрин или нет) */
+
+// let body = document.querySelector('body');
+
+// if (isMobile.any()) {
+//     body.classList.add('touchscreen');
+
+//     let arrow = document.querySelectorAll('.arrow');
+//     for (var i = 0; i < arrow.length; i++) {
+//         let thisLink = arrow[i].previousElementSibling;
+//         let subMenu = arrow[i].nextElementSibling;
+//         let thisArrow = arrow[i];
+
+//         thisLink.classList.add('parent');
+//         arrow[i].addEventListener('click', function() {
+//             subMenu.classList.toggle('open');
+//             thisArrow.classList.toggle('active')
+//         });
+//     }
+// } else {
+//     body.classList.add('mouse');
+// }
